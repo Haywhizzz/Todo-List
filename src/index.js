f46b58bd8235aaf861787b48339d86d6065f1941
enaburekhan <ericenaburekhan2018@gmail.com>
2021-01-31 00:20:48 +0100

    refactor code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import { Project, projectsArray, currentProject } from './project';
 import Task from './task';
 
 const $projectForm = document.getElementById('form-project');
+const feedback = document.querySelector(".feedback");
+
+const showFeedback = (text, action) => {
+  feedback.classList.add("showItem", `alert-${action}`);
+  feedback.innerHTML = `<p>${text}</p>`;
+
+  setTimeout(() => {
+    feedback.classList.remove("showItem", `alert-${action}`);
+  }, 3000);
+}
 
 $projectForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const $title = document.getElementById('project-name');
   if ($title.value === '') {
-    alert('The project must have a name');
+    showFeedback("project title cannot be empty", "danger");
     return;
   }
   const project = new Project($title.value);
@@ -26,17 +36,22 @@ $taskForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const $title = document.getElementById('task-name');
   if ($title.value === '') {
-    alert('The task must have a name');
+    showFeedback("task-title cannot be empty", "danger");
+    return;
+  }
+  const $description = document.getElementById('task-description');
+  if ($description.value === '') {
+    showFeedback("task-description cannot be empty", "danger");
     return;
   }
   const $date = document.getElementById('task-date');
-  if ($date.value === '') {
-    alert('Please choose a deadline');
+  if ($date.value === '') { 
+    showFeedback("Please choose a deadline", "danger");
     return;
   }
   const $priority = document.getElementById('task-priority');
 
-  const task = new Task($title.value, $date.value, $priority.value);
+  const task = new Task($title.value, $description.value, $date.value, $priority.value);
   currentProject.tasks.push(task);
   localStorage.setItem('list', JSON.stringify(task));
   task.render();
--- src/task.js
+++ src/task.js
@@ -4,8 +4,9 @@ import { currentProject } from './project';
 const $project = document.querySelector('#task-container');
 
 class Task {
-  constructor(name, date, priority) {
-    this.name = name;
+  constructor(title, description, date, priority) {
+    this.title = title;
+    this.description = description;
     this.date = date;
     this.priority = priority;
     this.id = setId();
@@ -15,10 +16,12 @@ class Task {
     const $div = createHtmlElement('div', this.id, ['task'], null);
 
     const $divTaskName = createHtmlElement('div', null, ['task-name'], null);
+    const $divTaskDescription = createHtmlElement('div', null, ['task-descrption'], null);
     const $checkbox = createHtmlElement('input', null, ['checkbox'], null);
     $checkbox.type = 'checkbox';
 
-    const $p = createHtmlElement('p', null, null, this.name);
+    const $p = createHtmlElement('p', null, null, this.title);
+    const $pi = createHtmlElement('p', null, null, this.description);
 
     const $divTaskInfo = createHtmlElement('div', null, ['task-info'], null);
     const $date = createHtmlElement('p', null, null, this.date);
@@ -34,17 +37,25 @@ class Task {
     $divTaskName.appendChild($checkbox);
     $divTaskName.appendChild($p);
 
+    $divTaskDescription.appendChild($checkbox);
+    $divTaskDescription.appendChild($pi);
+
     $divTaskInfo.appendChild($date);
     $divTaskInfo.appendChild($badge);
     $divTaskInfo.appendChild($delete);
     $divTaskInfo.appendChild($edit);
 
     $div.appendChild($divTaskName);
+    $div.appendChild($divTaskDescription);
     $div.appendChild($divTaskInfo);
 
     $project.appendChild($div);
     $checkbox.addEventListener('change', () => {
       $p.classList.toggle('done');
+    });
+    $project.appendChild($div);
+    $checkbox.addEventListener('change', () => {
+      $pi.classList.toggle('done');
     });
     if (this.priority === 'urgent') {
       $badge.classList.add('urgent');
@@ -69,18 +80,15 @@ class Task {
     $form.addEventListener('submit', (e) => {
       e.preventDefault();
       const $title = document.getElementById('task-name-edit');
-      if ($title.value === '') {
-        alert('The task must have a name');
-        return;
-      }
+
+      const $description = document.getElementById('task-description-edit');
+    
       const $date = document.getElementById('task-date-edit');
-      if ($date.value === '') {
-        alert('Please choose a deadline');
-        return;
-      }
+      
       const $priority = document.getElementById('task-priority-edit');
 
-      this.name = $title.value;
+      this.title = $title.value;
+      this.description = $description.value;
       this.date = $date.value;
       this.priority = $priority.value;
       $modal.style.display = 'none';
